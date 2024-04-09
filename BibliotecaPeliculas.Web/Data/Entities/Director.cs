5561cb5bcd04602bdf630b6d4020142ee775f6f7
EstebanPineda <estebanpineda527@gmail.com>
2024-04-08 23:08:51 -0500

    Creacion del proyecto y la clase director

---
+++ BibliotecaPeliculas.Web/Data/Entities/Director.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaPeliculas.Web.Data.Entities
+{
+    public class Director
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+    }
+}
