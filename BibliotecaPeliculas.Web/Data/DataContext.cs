3ca1add876361ec63ec207693b5df1cbda36d888
NELSON-PINZON\PC <nelmarpin@hotmail.com>
2024-04-10 19:11:30 -0500

    CRUD Usuario

---
+++ BibliotecaPeliculas.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using LibraryFilms.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryFilms.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Director> Directors { get; set; }  
+    }
+}
