e00b9cfd88025e44579494bd733c46ae46866250
NELSON-PINZON\PC <nelmarpin@hotmail.com>
2024-04-14 12:34:06 -0500

    Fución protecto director con usuario y roles

---
+++ BibliotecaPeliculas.Web/Controllers/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using LibraryFilms.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryFilms.Web.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public RolesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BibliotecaPeliculas.Web/Data/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryFilms.Web.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength(32, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+
+        public string Name { get; set; }
+        [MaxLength(32, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+
+        public string Password { get; set; }
+        
+        public Role Role { get; set; }
+
+    }
+}
