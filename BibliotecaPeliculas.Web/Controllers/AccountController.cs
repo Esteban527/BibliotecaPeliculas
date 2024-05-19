f4d8ba9cd4960aee576ab93ce7f9b47698a9bb8b
NELSON-PINZON\PC <nelmarpin@hotmail.com>
2024-05-18 23:05:05 -0500

    Crud de Section

--- BibliotecaPeliculas.Web/Controllers/AccountController.cs
+++ BibliotecaPeliculas.Web/Controllers/AccountController.cs
@@ -1,12 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using AspNetCoreHero.ToastNotification.Abstractions;
+//using LibraryFilms.Web.DTOs;
+//using LibraryFilms.Web.Services;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.ModelBinding;
 
-namespace LibraryFilms.Web.Controllers
-{
-    public class AccountController : Controller
-    {
-        public IActionResult Login()
-        {
-            return View();
-        }
-    }
-}
+//namespace LibraryFilms.Web.Controllers
+//{
+//    public class AccountController : Controller
+//    {
+//        private readonly IUsersService _usersService;
+//        private readonly INotyfService _noty;
+
+//        public AccountController(IUsersService usersService, INotyfService noty)
+//        {
+//            _usersService = usersService;
+//            _noty = noty;
+//        }
+
+//        [HttpGet]
+//        public IActionResult Login()
+//        {
+//            return View();
+//        }
+
+
+//        [HttpPost]
+//        public async Task<IActionResult> Login(LoginDTO dto)
+//        {
+//            if(ModelState.IsValid) 
+//            {
+//                Microsoft.AspNetCore.Identity.SignInResult result = await _usersService.LoginAsync(dto);
+//                if (result.Succeeded)
+//                {
+//                    if(Request.Query.Keys.Contains("ReturnUrl"))
+//                    {
+//                        return Redirect(Request.Query["ReturnUrl"].First());
+//                    }
+
+//                    return RedirectToAction("Dashboard, Home");
+//                }
+
+//                ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos");
+//                _noty.Error("Email o contraseña incorrectos");
+//            }
+//            return View(dto);
+//        }
+
+//        [HttpGet]
+//        public async Task<IActionResult> Logout()
+//        {
+//            await _usersService.LogoutAsync();
+//            return RedirectToAction("Login");
+//        }
+
+//    }
+//}
--- BibliotecaPeliculas.Web/Services/ISectionsService.cs
+++ BibliotecaPeliculas.Web/Services/ISectionsService.cs
@@ -1,16 +1,28 @@
 ﻿using LibraryFilms.Web.Core;
 using LibraryFilms.Web.Data;
 using LibraryFilms.Web.Data.Entities;
+using LibraryFilms.Web.Helpers;
+using LibraryFilms.Web.Requests;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol.Plugins;
 using System.Collections.Generic;
+
 
 namespace LibraryFilms.Web.Services
 {
     public interface ISectionsService
     {
         public Task<Response<Section>> CreateAsync(Section model);
+
         public Task<Response<List<Section>>> GetListAsync();
+
+        public Task<Response<Section>> GetOneAsync(int id);
+
+        public Task<Response<Section>> EditAsync(Section model);
+
+        public Task<Response<Section>> DeleteAsync(int id);
+
+        public Task<Response<Section>> ToggleSectionAsync(ToggleSectionRequest request);
     }
 
     public class SectionsService : ISectionsService
@@ -34,20 +46,11 @@ namespace LibraryFilms.Web.Services
                 await _context.AddAsync(section);
                 await _context.SaveChangesAsync();
 
-                return new Response<Section>
-                {
-                    IsSuccess = true,
-                    Message = "Sección creada con éxito",
-                    Result = section
-                };
+                return ResponseHelper<Section>.MakeResponseSuccess(section, "Sección creada con éxito");
             }
             catch (Exception ex) 
             {
-                return new Response<Section>
-                {
-                    IsSuccess = false,
-                    Message = ex.Message,
-                };
+                return ResponseHelper<Section>.MakeResponseFail(ex);
             }
         }
 
@@ -57,25 +60,93 @@ namespace LibraryFilms.Web.Services
             {
                 List<Section> list = await _context.Sections.ToListAsync();
 
-                return new Response<List<Section>> 
-                { 
-                    IsSuccess = true,
-                    Message = "Secciones obtenidas con éxito",
-                    Result = list
-                };
+                return ResponseHelper<List<Section>>.MakeResponseSuccess(list, "Secciones obtenidas con éxito");
 
             }
-
             catch (Exception ex)
             {
-                return new Response<List<Section>>
-                {
-                    IsSuccess = false,
-                    Message = ex.Message,
-                };
+                return ResponseHelper<List<Section>>.MakeResponseFail(ex);
             }
+        }
+        //---->>
+        public async Task<Response<Section>> GetOneAsync(int id)
+        {
+            try
+            {
+                Section? section = await _context.Sections.FirstOrDefaultAsync(s => s.Id == id);
 
-            throw new NotImplementedException();
+                if (section is null)
+                {
+                    return ResponseHelper<Section>.MakeResponseFail($"La sección con id '{id}' no existe.");
+                }
+
+                return ResponseHelper<Section>.MakeResponseSuccess(section);
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelper<Section>.MakeResponseFail(ex);
+            }
+        }
+        
+        public async Task<Response<Section>> EditAsync(Section model)
+        {
+            try
+            {
+                _context.Sections.Update(model);
+                await _context.SaveChangesAsync();
+
+                return ResponseHelper<Section>.MakeResponseSuccess(model, "Sección editada con éxito");
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelper<Section>.MakeResponseFail(ex);
+            }
+        }
+
+        public async Task<Response<Section>> DeleteAsync(int id)
+        {
+            try
+            {
+                Section? section = await _context.Sections.FirstOrDefaultAsync(s => s.Id == id);
+
+                if (section is null)
+                {
+                    return ResponseHelper<Section>.MakeResponseFail($"La sección con id '{id}' no existe.");
+                }
+
+                _context.Sections.Remove(section);
+                await _context.SaveChangesAsync();
+
+                return ResponseHelper<Section>.MakeResponseSuccess("Sección eliminada con éxito");
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelper<Section>.MakeResponseFail(ex);
+            }
+        }
+
+        public async Task<Response<Section>> ToggleSectionAsync(ToggleSectionRequest request)
+        {
+            try
+            {
+                Section? model = await _context.Sections.FindAsync(request.Id);
+
+                if (model == null)
+                {
+                    return ResponseHelper<Section>.MakeResponseFail($"No existe seeción con id '{request.Id}'");
+                }
+
+                model.IsHidden = request.Hide;
+
+                _context.Sections.Update(model);
+                await _context.SaveChangesAsync();
+
+                return ResponseHelper<Section>.MakeResponseSuccess("Sección Actualizada con éxito");
+            }
+            catch (Exception ex)
+            {
+                return ResponseHelper<Section>.MakeResponseFail(ex);
+            }
         }
     }
 }
